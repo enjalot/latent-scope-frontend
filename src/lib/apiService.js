0ad71ad1230e6b1525b673b1b27e323cc08d9628
Ian Johnson <enjalot@gmail.com>
2025-01-15 13:21:12 -0500

    dev mode hits dev api

--- src/lib/apiService.js
+++ src/lib/apiService.js
@@ -1,24 +1,23 @@
-const dev = import.meta.env.VITE_DEV ? "-dev" : ""
-console.log("DEV", dev)
+const dev = import.meta.env.MODE == "development" ? "-dev" : ""
 
 export const apiService = {
   getScope: async (userId, datasetId, scopeId) => {
-    return fetch(`https://enjalot--latent-scope-api-app-scope-meta-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}`).then((response) =>
+    return fetch(`https://enjalot--latent-scope-api-app-scope-meta${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}`).then((response) =>
       response.json()
     );
   },
   getRowsByIndices: async (userId, datasetId, scopeId, indices) => {
-    return fetch(`https://enjalot--latent-scope-api-app-rows-by-index-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&indices=${indices.toString()}`).then((response) =>
+    return fetch(`https://enjalot--latent-scope-api-app-rows-by-index${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&indices=${indices.toString()}`).then((response) =>
       response.json()
     );
   },
   getScopeRows: async (userId, datasetId, scopeId) => {
-    return fetch(`https://enjalot--latent-scope-api-app-scope-data-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}`).then((response) =>
+    return fetch(`https://enjalot--latent-scope-api-app-scope-data${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}`).then((response) =>
       response.json()
     );
   },
   columnFilter: async (userId, datasetId, scopeId, query) => {
-    return fetch(`https://enjalot--latent-scope-api-app-column-filter-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&query=${JSON.stringify(query)}`).then((response) =>
+    return fetch(`https://enjalot--latent-scope-api-app-column-filter${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&query=${JSON.stringify(query)}`).then((response) =>
       response.json()
     );
   },
@@ -27,7 +26,7 @@ export const apiService = {
     // TODO: this should be a util function? converting the model to the HF name
     const modelId = scope.embedding?.model_id.replace("___", "/").split("-").slice(1).join("-")
     console.log("MODEL ID", modelId)
-    return fetch(`https://enjalot--latent-scope-api-app-nn-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&model=${modelId}&query=${query}`)
+    return fetch(`https://enjalot--latent-scope-api-app-nn${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&model=${modelId}&query=${query}`)
       .then((response) => response.json())
       .then((data) => {
         let dists = [];
@@ -43,7 +42,7 @@ export const apiService = {
       });
   },
   searchSaeFeature: async (userId, datasetId, scopeId, featureId, threshold) => {
-    return fetch(`https://enjalot--latent-scope-api-app-feature-dev.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&feature=${featureId}&threshold=${threshold}`).then((response) =>
+    return fetch(`https://enjalot--latent-scope-api-app-feature${dev}.modal.run/?db=${userId}/${datasetId}&scope=${scopeId}&feature=${featureId}&threshold=${threshold}`).then((response) =>
       response.json()
     );
   },
