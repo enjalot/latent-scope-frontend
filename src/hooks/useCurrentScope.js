92902f295b4af949b03739d560982e19e5c540e4
Ian Johnson <enjalot@gmail.com>
2025-01-16 16:11:37 -0500

    better SAE check for scope

--- src/hooks/useCurrentScope.js
+++ src/hooks/useCurrentScope.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback, useMemo } from 'react';
 import { apiService } from '../lib/apiService';
-
+import { saeAvailable } from '../lib/SAE';
 const useCurrentScope = (userId, datasetId, scopeId) => {
     const [scope, setScope] = useState(null);
     const [dataset, setDataset] = useState(null);
@@ -9,9 +9,14 @@ const useCurrentScope = (userId, datasetId, scopeId) => {
     useEffect(() => {
         apiService.getScope(userId, datasetId, scopeId).then((scope) => {
             console.log("scope", scope)
+            if(saeAvailable[scope.embedding?.model_id]) {
+                setSae(scope.sae);
+            } else {
+                delete scope.sae
+                delete scope.sae_id
+            }
             setScope(scope);
             setDataset(scope.dataset);
-            setSae(scope.sae);
         });
     }, [userId, datasetId, scopeId]);
 
