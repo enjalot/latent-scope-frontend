9add32ba6bafd32696ecee2b9fb94178f24d6b6e
Ian Johnson <enjalot@gmail.com>
2025-01-16 15:03:24 -0500

    try again

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 const mode = process.env.MODE || 'development';
 
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'production' ? '/latent-scope-frontend/' : '/',
-  base: '/',
+  base: mode === 'production' ? `https://latent.estate/` : '/',
+  // base: '/',
   plugins: [react(), wasm(), topLevelAwait()],
   build: {
     outDir: `dist/${mode}`,
