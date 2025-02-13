49469ef7f70cac1ccb743037e68b0b75853c4777
Jimmy Zhang <jzhang621@gmail.com>
2025-02-13 17:47:07 -0500

    some dark mode styles

--- src/components/Explore/Search/SuggestionsPanel.jsx
+++ src/components/Explore/Search/SuggestionsPanel.jsx
@@ -1,19 +1,29 @@
 // SuggestionsPanel.jsx
 import React from 'react';
-import styles from './SuggestionsPanel.module.css';
+import PropTypes from 'prop-types';
+import styles from './SuggestionsPanel.module.scss';
 
 /*
  * SuggestionsPanel displays a list of suggestions when there is no active search query.
  * Each suggestion is clickable, triggering the onSelect callback to update the query.
  */
-const SuggestionsPanel = ({ suggestions, onSelect }) => (
-  <div className={styles.suggestionsPanel}>
-    {suggestions.map((suggestion, idx) => (
-      <div key={idx} onClick={() => onSelect(suggestion)}>
-        {suggestion}
-      </div>
-    ))}
-  </div>
-);
+const SuggestionsPanel = ({ suggestions, onSelect }) => {
+  return (
+    <div className={styles.suggestionsList}>
+      {suggestions.map((suggestion, index) => (
+        <div key={index} className={styles.suggestionRow} onClick={() => onSelect(suggestion)}>
+          <div className={styles.suggestionContent}>
+            <span>{suggestion}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+SuggestionsPanel.propTypes = {
+  suggestions: PropTypes.array.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 export default SuggestionsPanel;
